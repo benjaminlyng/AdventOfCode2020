e721b7216fbcb499ede7f5921d89e03e1e05c931
Benjamin L. J <benjamin@lyng.space>
2020-12-19 06:37:17 +0100

    part 2

--- day18.py
+++ day18.py
@@ -16,8 +16,7 @@ def do_math(homework:str):
     
     if isinstance(homework, re.Match) :
         homework = homework.group()
-    # print(homework)
-    
+        
     homework = re.sub(r'\([\d+* ]*\)', lambda x : do_math(x.group()[1:-1]), homework)
     
     homework = re.sub(r'\([\d+* ]*\)', do_math, homework)   
@@ -25,6 +24,34 @@ def do_math(homework:str):
     homework = re.sub(r'^\d* [\d+* ]* \d*', lambda x: do_math(x.group()), homework)
     return homework
 
-print(f"{sum(int(do_math(i)) for i in l)=}")
+print(f"Part 1: {sum(int(do_math(i)) for i in l)=}")
 
 
+def do_math2(homework:str):
+    """ Do wierd math so that '+' evaluates before '*'
+
+    >>> do_math2('2 * 3 + (4 * 5)')
+    '46'
+    >>> do_math2('5 + (8 * 3 + 9 + 3 * 4 * 3)')
+    '1445'
+    >>> do_math2('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
+    '669060'
+    >>> do_math2('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
+    '23340'
+    """
+    
+    if isinstance(homework, re.Match) :
+        homework = homework.group()
+    # print(homework)
+    
+    homework = re.sub(r'\([\d+* ]*\)', lambda x : do_math2(x.group()[1:-1]), homework)
+    
+    homework = re.sub(r'\([\d+* ]*\)', do_math2, homework)   
+    homework = re.sub(r'\d+ [+] \d+', lambda x: str(eval(x.group())), homework)
+    homework = re.sub(r'\d+ [\d+ ]+ \d+', lambda x: do_math2(x.group()), homework)
+
+    homework = re.sub(r'\d+ [*] \d+', lambda x: str(eval(x.group())), homework)
+    homework = re.sub(r'\d+ [\d* ]+ \d+', lambda x: do_math2(x.group()), homework)
+    return homework
+
+print(f"Part 2: {sum(int(do_math2(i)) for i in l)=}")
